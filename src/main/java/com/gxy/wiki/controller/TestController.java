9085d6d29787362831979e50c6161bbf9848f131
guo <904004593@qq.com>
2021-07-21 20:37:31 +0800

    1.0.4--配置热部署

--- src/main/java/com/gxy/wiki/controller/TestController.java
+++ src/main/java/com/gxy/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.gxy.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author GUO
  * @Classname TestController
- * @Description TODO
+ * @Description 测试接口
  * @Date 2021/7/21 15:50
  */
 @RestController //返回JSON字符串 =@ResponseBody+@Controller
 //@ResponseBody 返回json对象
 //@Controller 返回一个页面
 public class TestController {
+
+    @Value("${test.hello}")
+    private String testHello;
     /**
      * GET,POST,PUT,DELETE
      * /user?id=1
@@ -21,13 +25,13 @@ public class TestController {
      *
      * @return
      */
-    @GetMapping("/hello")
 //    @PostMapping
 //    @PutMapping
 //    @DeleteMapping
 //    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @GetMapping("/hello")
     public String hello(){
-        return "hello API";
+        return "hello API! "+testHello;
     }
     @PostMapping("/helloPost")
     public String helloPost(String name){
