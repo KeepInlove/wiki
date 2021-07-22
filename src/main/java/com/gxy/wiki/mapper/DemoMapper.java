5f14ce7fd2baa5ae6834bf8f782e971e9e445b0a
guo <904004593@qq.com>
2021-07-22 10:02:11 +0800

    1.0.6--集成mybatis generator,生成demo表代码

---
+++ src/main/java/com/gxy/wiki/mapper/DemoMapper.java
@@ -0,0 +1,31 @@
+package com.gxy.wiki.mapper;
+
+import com.gxy.wiki.domain.Demo;
+import com.gxy.wiki.domain.DemoExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    Demo selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
+}
