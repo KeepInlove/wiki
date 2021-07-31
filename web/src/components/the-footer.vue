430479654ae2d4867578889c99860f04934e494c
guo <904004593@qq.com>
2021-07-31 18:31:23 +0800

    2.0.1--设计前端布局,使用Ant Design Vue

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        Spring Boot + Vue 学习项目 ©2021 Created by Guo
+    </a-layout-footer>
+</template>
+
+<script>
+    export default {
+        name: "the-footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
